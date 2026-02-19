df60fecdcf4a01e3183325a83bb143a1c3c72b6b
José Yáñez <jlyp_sn@protonmail.com>
2026-02-18 23:53:31 -0600

    Implement basic algorithm to display books.

--- script.js
+++ script.js
@@ -12,4 +12,46 @@ function addBookToLibrary (title, author, pages, isRead) {
     myLibrary.push(book);
 }
 
+function deployLibrary () {
+    bookshelf.innerHTML = null;
+    for (let i = 0; i < myLibrary.length; i++) {
+
+        const newItem = document.createElement("div");
+        newItem.setAttribute("class", "book-item");
+        newItem.setAttribute("id", myLibrary[i].id);
+
+        const itemTitle = document.createElement('div');
+        itemTitle.setAttribute("class", 'book-title');
+        itemTitle.textContent = myLibrary[i].title;
+
+        const itemAuthor = document.createElement('div');
+        itemAuthor.setAttribute("class", 'book-author');
+        itemAuthor.textContent = "by " + myLibrary[i].author;
+
+        const itemPages = document.createElement('div');
+        itemPages.setAttribute("class", 'book-pages');
+        itemPages.textContent = myLibrary[i].pages + " pages";
+
+        const itemStatus = document.createElement('div');
+        itemStatus.setAttribute("class", 'book-status');
+        if (myLibrary[i].isRead) {
+            itemStatus.textContent = "Read";           
+        } else {
+            itemStatus.textContent = "Not read";
+        }
+
+        newItem.appendChild(itemTitle);
+        newItem.appendChild(itemAuthor);
+        newItem.appendChild(itemPages);
+        newItem.appendChild(itemStatus);
+
+        bookshelf.appendChild(newItem);
+
+    }
+}
+
 const myLibrary = [];
+
+const bookshelf = document.getElementById("bookshelf");
+
+
